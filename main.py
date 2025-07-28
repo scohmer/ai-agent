82bb5609754acb0741c527a77c8f2a971c8f5891
Sean Cohmer <sean@Seans-MacBook-Air.local>
2025-07-28 17:30:09 -0400

    verbose

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 # Load environment variables from .env
 load_dotenv()
@@ -9,8 +10,14 @@ api_key = os.environ.get("GEMINI_API_KEY")
 
 # Check if a prompt was provided
 if len(sys.argv) < 2:
-    print("Usage: python3 main.py \"<your prompt here>\"")
+    print("Usage: python3 main.py \"<your prompt here>\" [--verbose]")
     sys.exit(1)
+
+# Check for --verbose flag
+verbose = False
+if "--verbose" in sys.argv:
+    verbose = True
+    sys.argv.remove("--verbose")
 
 # Define prompt
 prompt = " ".join(sys.argv[1:])
@@ -18,15 +25,22 @@ prompt = " ".join(sys.argv[1:])
 # Create Gemini client
 client = genai.Client(api_key=api_key)
 
+# Create conversation
+messages = [
+    types.Content(role="user", parts=[types.Part(text=prompt)]),
+]
+
 # Generate content
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 
 # Print response text
 print("Response:\n", response.text)
 
-# Print token usage
-print(f"\nPrompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if verbose:
+    print(f"\nUser prompt: {prompt}")
+    # Print token usage
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 def main():
     return
